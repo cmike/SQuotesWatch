4b36f46da237d5869eec453b33f57a1d040d3fdf
Mike Youk <muu_muu2000@yahoo.com>
2012-04-19 21:22:45 +0300

    More Prefs UI

--- src/com/muustwatch/Prefs.java
+++ src/com/muustwatch/Prefs.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.widget.TimePicker;
+import android.widget.ToggleButton;
 
 public class Prefs extends Activity {
 	// TODO below are constant definition to be replaced by real code
@@ -17,6 +18,10 @@ public class Prefs extends Activity {
 	// TODO Eof temporary constant definition
 	boolean is24;
 	TimePicker StartTime, StopTime;
+	ToggleButton[] UI_WeekDays = new ToggleButton[7];
+	int[] UI_WeekDays_IDS = {R.id.toggleButton1, R.id.toggleButton2, R.id.toggleButton3, 
+				R.id.toggleButton4, R.id.toggleButton5, R.id.toggleButton6, R.id.toggleButton7};
+	int[] LocalizedWeekDayIDs = new int[7];
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,6 +31,12 @@ public class Prefs extends Activity {
         
         Calendar this_calend = Calendar.getInstance();
         int first_day_a_week = this_calend.getFirstDayOfWeek();
+        
+        int _dayID, _i;
+        for (_dayID = first_day_a_week, _i = 0; _dayID <= Calendar.SATURDAY; _dayID++, _i++)
+        	LocalizedWeekDayIDs[_i] = _dayID;
+        for (_dayID = Calendar.SUNDAY; _dayID < first_day_a_week; _dayID++, _i++)
+        	LocalizedWeekDayIDs[_i] = _dayID;
         
         DateFormatSymbols symbols = new DateFormatSymbols(); 
         String[] dayNames = symbols.getShortWeekdays();
@@ -38,10 +49,17 @@ public class Prefs extends Activity {
         
         MUUDebug.Log(class_nm, "Days = " + dayNames.length);
         MUUDebug.Log(class_nm, "First Day = " + first_day_a_week);
-        for (int idx=1; idx < dayNames.length; idx++) {
-           MUUDebug.Log(class_nm, dayNames[idx]);
+        for (int idx=0; idx < 7; idx++) {
+           MUUDebug.Log(class_nm, dayNames[LocalizedWeekDayIDs[idx]]);
         }
         
+        for (_i = 0; _i < 7; _i++) {
+          UI_WeekDays[_i] = (ToggleButton)findViewById(UI_WeekDays_IDS[_i]);
+          UI_WeekDays[_i].setTextOn(dayNames[LocalizedWeekDayIDs[_i]].subSequence(0, 3));
+          UI_WeekDays[_i].setTextOff(dayNames[LocalizedWeekDayIDs[_i]].subSequence(0, 3));
+          
+          UI_WeekDays[_i].setChecked(true);
+        }
         StartTime = (TimePicker)findViewById(R.id.StartTime);
         StopTime = (TimePicker)findViewById(R.id.StopTime);
 
